da0b80dd7d21828b60cb6b5a55ec75055b355a4c
Andrew <andrew.ryabchenko@gmail.com>
2023-12-11 21:15:14 -0500

    Work in progress

--- api.py
+++ api.py
@@ -5,6 +5,7 @@ import httpx
 from exceptions import APIError
 from threading import Thread
 import openai
+from time import sleep
 
 class ApiCall(Thread):
 
@@ -15,17 +16,15 @@ class ApiCall(Thread):
     response_format: str
     speed: float
     model: str
-    #Rate limit information
-    limit_requests: int
-    remaining_requests: int
-    reset_requests: str
 
     def __init__(self, data: str, group: None = None, 
                  target: Callable[..., object] | None = None,
                  name: str | None = None, args: Iterable[Any] = ..., 
                  kwargs: Mapping[str, Any] | None = None, *, 
                  daemon: bool | None = None) -> None:
+        #Text data for current API call
         self.data = data
+        #Call parent class constructor
         super().__init__(group, target, name, args, kwargs, daemon=daemon)
         
     def get_limits():
@@ -59,11 +58,21 @@ class ApiCall(Thread):
             return len_text / 1000 * 0.015
         return len_text / 1000 * 0.03
 
+    def check_rpd(self, headers) -> bool:
+        """Check whether the RPD account limit has exceeded."""
+
+        limit_requests = int(headers["x-ratelimit-limit-requests"])
+        remaining_requests = int(headers["x-ratelimit-remaining-requests"])
+        if limit_requests == 200 and remaining_requests < 1:
+            #Free tier account. RPD limit exceeded.
+            return True
+        
+        return False
+      
     def run(self) -> None:
         
         client = openai.OpenAI(
-            api_key=ApiCall.api_key,
-            max_retries=3,
+            api_key=ApiCall.api_key
         )
         
         try:
@@ -74,9 +83,26 @@ class ApiCall(Thread):
                 response_format=ApiCall.response_format,
                 speed=ApiCall.speed,
             )
+            
             self.error = False
             self.speech = response.read()
-            
+        
+        #Rate limit exception handler
+        except openai.RateLimitError as err:
+            headers = err.response.headers
+            if self.check_rpd(headers):
+                #RPD limit is exceeded.
+                self.error = True
+                self.status_code = err.status_code
+                self.response = err.response
+                self.speech = None
+            else:
+                #RPM is likely exceeded. Re-try again in 60s.
+                print("RPM limit exceeded. Re-trying in 60s.")
+                sleep(60.0)
+                self.run()
+                
+        #All other types of API or network related exceptions   
         except openai.APIError as err:
             self.error = True
             self.status_code = err.status_code
@@ -86,11 +112,11 @@ class ApiCall(Thread):
 if __name__ == "__main__":
     from segment_generator import SegmentGenerator
     
-    API_KEY_TIER1 = "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"
+    API_KEY_TIER1 = "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"
     
     ApiCall.api_key = API_KEY_TIER1
     ApiCall.model = "tts-1"
-    ApiCall.voice = "echo"
+    ApiCall.voice = "onyx"
     ApiCall.response_format = "mp3"
     ApiCall.speed = 1.0
     
@@ -98,13 +124,13 @@ if __name__ == "__main__":
     
     input_file_path = "testing/input.txt"
     
-    infile = open(input_file_path)
+    infile = open(input_file_path,encoding="utf-8")
     
-    sg = SegmentGenerator(4096, infile)
+    sg = SegmentGenerator(500, infile)
 
     sg.generate_sentences()
     sg.generate_segments()
-
+    print(sg.num_segments)
     for segment in sg.segments:
         api_call = ApiCall(segment)
         api_call.start()
@@ -117,9 +143,3 @@ if __name__ == "__main__":
                 outfile.write(api_call.speech)
             else:
                 print(f"Status code: {api_call.status_code}\nMessage: {api_call.response}")
-        
-        
-    
-    
-    
-    
